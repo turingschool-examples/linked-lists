18aab991d0a56e2ccc682c1b78a3ccc5bda10fdf
Brittany Storoz <brittanystoroz@gmail.com>
2018-04-02 09:49:45 -0600

    Add empty files and update tests

--- tests/Node-test.js
+++ tests/Node-test.js
@@ -1,23 +1,22 @@
 import { expect } from 'chai';
-import Node from '../scripts/Node'
+import Node from '../scripts/Node';
 
 describe('NODE', () => {
   let node;
 
   beforeEach(() => {
-    node = new Node('pizza')
+    node = new Node('pizza');
   })
 
   it('should be a thing', () => {
-    expect(node).to.exist
+    expect(node).to.exist;
   })
 
   it('should default next to null', () => {
     expect(node.next).to.equal(null);
-    expect(node.prev).to.equal(null);
   })
 
   it('should take data and assign it to data prop', () => {
-    expect(node.data).to.equal('pizza')
+    expect(node.data).to.equal('pizza');
   })
 })
